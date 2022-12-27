2a80ab46a7203693e1b8666abcfaa09c24e7f029
IlyaChistogov <115698573+IlyaChistogov@users.noreply.github.com>
2022-12-28 00:08:34 +0500

    Среднее значение

--- Lab3.py
+++ Lab3.py
@@ -5,3 +5,6 @@ pyDatalog.create_terms('X, Z, Y, Sum, div, Average, y, SumRand')
 
 (y[X] == sum_(Y, for_each=Y)) <= ((Y._in(range_(X+1))))
 print(y[10] == Sum)
+
+(div[X, Y] == Z) <= (X // Y == Z)
+print(div[y[10], 10] == Average)
